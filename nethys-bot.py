1b6be02f59cc94509f01b9d2f74d20fd29fe369b
Zixus <gaudensius.dimas@gmail.com>
2019-09-26 21:15:16 +0700

    debug proxy message

--- nethys-bot.py
+++ nethys-bot.py
@@ -261,6 +261,6 @@ class NethysClient(discord.Client):
 					else:
 						await channel.send(embed=discord.Embed(description="Cannot find `" + search + "` in `" + command + "`"))
 		elif (message.channel.id == 626777118923030558):
-			await self.get_guild(447398596455694337).get_channel(459699764049477643).send(message.content)
+			await self.get_guild(588654434582790169).get_channel(599588535850237972).send("*peluk <@" + str(self.get_guild(588654434582790169).get_member(209048672195969025)) + ">*")
 client = NethysClient()
 client.run(TOKEN)
