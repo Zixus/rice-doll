2240b238a8fa104086d53dcd6f19a1bb3b351a09
Zixus <gaudensius.dimas@gmail.com>
2019-09-27 14:40:55 +0700

    debug embed by others

--- nethys-bot.py
+++ nethys-bot.py
@@ -210,8 +210,11 @@ class NethysClient(discord.Client):
 
 		elif ((message.channel == self.dest_channel) and self.is_proxy_active):
 			if (message.author != self):
-				for embedmsg in message.embeds:
-					await self.proxy_channel.send(content="`prox " + str(message.id) + "`\n<@" + str(message.author.id) + "> : " + message.content, embed=embedmsg)
+				if (len(message.embeds) > 0):
+					for embedmsg in message.embeds:
+						await self.proxy_channel.send(content="`prox " + str(message.id) + "`\n<@" + str(message.author.id) + "> : " + message.content, embed=embedmsg)
+				else:
+					await self.proxy_channel.send("`prox " + str(message.id) + "`\n<@" + str(message.author.id) + "> : " + message.content)	
 			else:
 				await self.proxy_channel.send("`prox " + str(message.id) + "`\n<@" + str(message.author.id) + "> : " + message.content)
 
