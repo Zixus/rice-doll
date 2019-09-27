110d79e7bb025aafe93117de995264949203ba02
Zixus <gaudensius.dimas@gmail.com>
2019-09-27 13:44:23 +0700

    debug

--- nethys-bot.py
+++ nethys-bot.py
@@ -210,6 +210,8 @@ class NethysClient(discord.Client):
 
 		elif ((message.channel == self.dest_channel) and self.is_proxy_active):
 			await self.proxy_channel.send("`prox " + str(message.id) + "`\n<@" + str(message.author.id) + "> : " + message.content)
+			for embedmsg in message.embeds:
+				await self.proxy_channel.send(embed=embedmsg)
 
 		if (message.content.startswith(self.prefix)):
 			channel = message.channel
