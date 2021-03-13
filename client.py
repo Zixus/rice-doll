278f8d0a464fb27cd5b0ce77e4d7c5eb16424120
Zixus <gaudensius.dimas@gmail.com>
2021-03-14 01:47:36 +0700

    changed prefix to r!

--- client.py
+++ client.py
@@ -6,17 +6,17 @@ import discord
 import d20
 
 class GohanClient(discord.Client):
-	prefix = "/"
+	prefix = "r!"
 
 	async def on_ready(self):
-		await self.change_presence(activity=discord.Game("/help"))
+		await self.change_presence(activity=discord.Game("r!help"))
 		print('Logged on as {0}'.format(self.user))
 		
 	async def on_message(self, message):
 		content = "<@{}> : ".format(message.author.id)
 		try:
 			if (message.content.startswith(self.prefix)):
-				args = message.content[1:].split(" ", 1)
+				args = message.content[len(self.prefix):].split(" ", 1)
 				if (args[0] == "r") or (args[0] == "roll"):
 					args = args[1].split("#", 1)
 					if len(args) > 1:
@@ -25,7 +25,7 @@ class GohanClient(discord.Client):
 					content += str(result)
 					await message.channel.send(content)
 				elif (args[0] == "h") or (args[0] == "help"):
-					await message.channel.send(embed=discord.Embed(description="Usage: `/roll <syntax>` or `/r <syntax>`\n Refer to https://pypi.org/project/d20/ for the rolling syntax"))
+					await message.channel.send(embed=discord.Embed(description="Usage: `r!roll <syntax>` or `r!r <syntax>`\n Refer to https://pypi.org/project/d20/ for the rolling syntax"))
 
 		except Exception as e:
 			content += "\n" + str(type(e)) + ':\n' + str(e)
