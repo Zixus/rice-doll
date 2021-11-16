22ced313d642eb1b029e325f84973cfbf023e2f5
IqrarAminullah <Iqrar.silveria@gmail.com>
2021-11-16 23:41:55 +0700

    added alias

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
-from gohanclient import GohanClient
 import discord
+from gohanclient import GohanClient
+from discord.ext import commands
 
 bot = GohanClient()
 
-@bot.command()
+@bot.command(aliases=['h'])
 async def help(ctx):
     await ctx.send(embed=discord.Embed(description=bot.help()))
 
-@bot.command()
+@bot.command(aliases=['r'])
 async def roll(ctx, *args):
     message = "<@{}> : ".format(ctx.author.id)+str(bot.roll(args))
     await ctx.send(message)
